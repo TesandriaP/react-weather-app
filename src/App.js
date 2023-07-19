bc03844bdb6009c4af1402fd7b9d40e457b4aa47
TesandriaP <130936656+TesandriaP@users.noreply.github.com>
2023-07-19 19:04:39 -0400

    added gh url, deleted logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+      This project is made by Penn626 and is {""}
+     <a href="https://github.com/TesandriaP/react-weather-app" target="_blank">open-sourced on github</a> 
+     </footer>
+     
     </div>
   );
 }
