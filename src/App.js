b08df676a5f95e4a87941567cbf264aadd98779e
TesandriaP <130936656+TesandriaP@users.noreply.github.com>
2023-07-23 16:46:26 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-    <Weather />
+    <Weather 
+    defaultcity="Toronto"
+    />
      <footer>
       This project is made by Penn626 and is {""}
      <a href="https://github.com/TesandriaP/react-weather-app" target="_blank" rel='noreferrer'>open-sourced</a>  on GitHub.
