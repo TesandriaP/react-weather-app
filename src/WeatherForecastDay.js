463fcb3de26ab28c9f110dd536024ad6e7d5f4c4
TesandriaP <130936656+TesandriaP@users.noreply.github.com>
2023-07-29 18:38:44 -0400

    update

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -15,18 +15,17 @@ export default function WeatherForecastDay(props){
     function day(){
         let date= new Date(props.data.dt * 1000);
         let day= date.getDay();
-
         let days= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
         return days[day];
     }
 
     return(
-     <div>
-    <div className="WeatherForecast-day">{day()}</div>
-     <WeatherIcon code={props.data.weather[0].icon} size={36} /> 
-    <div className="WeatherForecast-temps"> <span className="WeatherForecast-temp-max">{maxTemp}°</span>
-    <span className="WeatherForecast-temp-min">{minTemp}°
+     <div className="WeatherForecast-day">
+    <div className="forecast-time">{day()}</div>
+     <WeatherIcon code={props.data.condition.icon} size={38} /> 
+    <div className="forecast-temps"> <span className="forecast-temp-max">{maxTemp}°</span>
+    <span className="forecast-temp-min">{minTemp}°
             </span>
          </div> 
      </div>
